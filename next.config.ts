8478f907bf8af7465d10cc1d97bbd056a241a081
CodeForgeNet <codeforgenet@icloud.com>
2025-05-29 18:26:50 +0530

    update next config file

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,9 @@
-// import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
-//   images: {
-//     domains: ["cdn-icons-png.flaticon.com"],
-//   },
-// };
-
-// export default nextConfig;
-
-const nextConfig = {
+const nextConfig: NextConfig = {
+  /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn-icons-png.flaticon.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
+    domains: ["cdn-icons-png.flaticon.com"],
   },
 };
 
