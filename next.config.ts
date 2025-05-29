7c0921db3043e234a257ebb8d5eac5e17692728f
CodeForgeNet <codeforgenet@icloud.com>
2025-05-29 18:04:06 +0530

    add netlify

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,25 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  images: {
-    domains: ["cdn-icons-png.flaticon.com"],
-  },
-};
-
-export default nextConfig;
-
 // import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
+//   /* config options here */
 //   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "cdn-icons-png.flaticon.com",
-//         port: "",
-//         pathname: "/**",
-//       },
-//     ],
+//     domains: ["cdn-icons-png.flaticon.com"],
 //   },
 // };
 
 // export default nextConfig;
+
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn-icons-png.flaticon.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
